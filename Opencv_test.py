1a7dea2c37aa97ac75e391dda2802c823f0ea2bd
Knightsll <305725977@qq.com>
2018-12-29 20:10:53 +0800

    d
    Signed-off-by: Knightsll <305725977@qq.com>

--- Opencv_test.py
+++ Opencv_test.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
 from scipy.misc import imsave
 #choose image
 imagepath = input("Please give me the image path:",)
@@ -8,10 +9,11 @@ imagepath = input("Please give me the image path:",)
 #get face parameters from xml file
 face_cascade = cv2.CascadeClassifier(r'/home/deep/anaconda3/envs/sound-recognize/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml')
 
+
+
 #read image
 image = cv2.imread(imagepath)
 gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 
 #scan the face from the image
 faces = face_cascade.detectMultiScale(
@@ -26,7 +28,8 @@ print("\n")
 print("find {0} faces!".format(len(faces)))
 
 for(x, y, w, h) in faces:
-    cv2.circle(image, (np.int(((x+x+w)/2)), np.int((y+y+h)/2)), np.int(w/2), (0, 255, 0), 2)
+    cv2.circle(image, (np.int(((x+x+w)/2)), np.int((y+y+h)/2)), np.int(w/2), (255, 255, 255), 3)
+
 plt.imshow(image)
 plt.show()
-imsave(r"./output.jpg", image)
+cv2.imwrite(r"./output.jpg", image)
--- opencv_test2.py
+++ opencv_test2.py
@@ -1,35 +1,58 @@
-#!/usr/bin/env python
 import cv2
-from distutils.version import LooseVersion
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-if LooseVersion(cv2.__version__).version[0] == 2:
-    pass
-else:
-    #choose image
-    imagepath = raw_input("Please give me the image path:",)
-           
-    #get face parameters from xml file
-    xmlpath = '/opt/ros/kinetic/share/OpenCV-3.3.1-dev/haarcascades/haarcascade_frontalface_default.xml'
-    face_cascade = cv2.CascadeClassifier(xmlpath)
-    #read image
-    image = cv2.imread(imagepath)
-    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+from scipy.misc import imsave
 
-    #scan the face from the image
-    faces=face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.15,
-        minNeighbors=10,
-        minSize=(50, 50)
-    )
+'''
+#choose image
+imagepath = input("Please give me the image path:",)
 
-    print "\n"
-    print "find {0} faces!".format(len(faces))
+#get face parameters from xml file
+face_cascade = cv2.CascadeClassifier(r'/home/deep/anaconda3/envs/sound-recognize/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml')
 
-    for(x,y,w,h) in faces:
-        cv2.circle(image,((x+x+w)/2,(y+y+h)/2),w/2,(0,255,0),2)
-    
-    cv2.imwrite("/home/ros-test/test_opencv.jpg",image)
+
+
+#read image
+image = cv2.imread(imagepath)
+gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#scan the face from the image
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.35,
+    minNeighbors=5,
+    minSize=(6, 6),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+print("\n")
+print("find {0} faces!".format(len(faces)))
+
+for(x, y, w, h) in faces:
+    cv2.circle(image, (np.int(((x+x+w)/2)), np.int((y+y+h)/2)), np.int(w/2), (255, 255, 255), 3)
+'''
+
+#打开摄像头的方法，window_name为显示窗口名，video_id为你设备摄像头的id，默认为0或-1，如果引用usb可能会改变为1，等
+def openvideo(window_name ,video_id):
+
+    cap=cv2.VideoCapture(video_id) # 获取摄像头
+    while cap.isOpened():
+        ok,frame=cap.read() # ok表示摄像头读取状态，frame表示摄像头读取的图像
+        if not ok :
+            break
+
+        plt.imshow(window_name,frame) # 将图像矩阵显示在一个窗口中
+        time.sleep(0.01)
+        if  0xFF==ord('q'): # 按键q后break
+            break
+
+    # 释放资源
+    cap.release()
+    print("cam closed")
+openvideo('mycam' ,0)
+
     
     
     
